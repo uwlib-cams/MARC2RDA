fd89af446e7aeb143361a3da19847567c2ff127e
Gerontakos <tgis@uw.edu>
2022-04-01 20:34:11 -0700

    Add directory Working Documents/Non-Mapping Documents/conditions with a script to extract conditions for the mapping documents for analysis and determining best practices for structuring the conditions

---
+++ Working Documents/Non-Mapping Documents/conditions/extractConditions.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+import csv
+
+def createMainSheet(fileList):
+    csvList = []
+    for f in fileList:
+        csvList.append(pd.read_csv(f, sep='\t', low_memory=False, encoding= 'unicode-escape'))
+    csvMerged = pd.concat(csvList, ignore_index=True)
+    sourceFile = csvMerged.to_csv('mergedSheet.csv', index=False)
+
+def createConditionSheet(merged):
+    with open(merged, newline='') as sheet:
+        sheet_reader = csv.DictReader(sheet, delimiter=',')
+        with open('conditionSheet.csv', mode='w') as final:
+            fieldnames=['Field', 'condition1', 'c1-values', 'condition2', 'c2-values']
+            writer = csv.DictWriter(final, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in sheet_reader:
+                if row['MARCTagCondition1']:
+                    writer.writerow({'Field': row['MARCField'], 'condition1': row['MARCTagCondition1'], 'c1-values': row['Condition1Values'], 'condition2': row['MARCTagCondition2'], 'c2-values': row['Condition2Values']})
+
+createMainSheet(['../../1XX.txt', '../../2XX.txt', '../../3XX.txt', '../../4XX.txt', '../../5XX.txt', '../../6XX.txt', '../../7XX.txt', '../../8XX.txt', '../../LDR-DIR.txt'])
+createConditionSheet('mergedSheet.csv')
---
+++ Working Documents/Non-Mapping Documents/conditions/readme.txt
@@ -0,0 +1,15 @@
+This folder:
+MARC2RDA\Working Documents\Non-Mapping Documents\conditions
+
+...contains tools to analyze the conditions entered in the mapping documents.
+
+The working report is conditionSheet.xlsx
+
+It is created as follows:
+
+1. run extractConditions.py
+2. open conditionSheet.csv in Excel
+    2a. sort as appropriate
+          2a1. especially helpful to eliminate empty rows
+    2b. Filter for unique records only
+    2c. save as conditionSheet.xlsx
