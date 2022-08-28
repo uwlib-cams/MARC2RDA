98d8c67cc53c5bc3db9026101880aee2837b94a8
Gerontakos <tgis@uw.edu>
2022-08-27 20:34:06 -0700

    add all changes to spreadsheets; 34 files changed

--- Working Documents/Non-Mapping Documents/zippedExcel2csv.py
+++ Working Documents/Non-Mapping Documents/zippedExcel2csv.py
@@ -17,7 +17,7 @@ import pandas as pd
 #7. Go to git repo and manually add, commit, push
 
 downloadPath = r"C:\Users\geron\Downloads\\"
-downloadFile = "drive-download-20220706T151552Z-001.zip"
+downloadFile = "drive-download-20220828T032418Z-001.zip"
 extractPath = r"C:\Users\geron\OneDrive\Desktop\csvStage_folders\\"
 dir = r"C:\Users\geron\OneDrive\Desktop\csvStage_files"
 gitRepo = r"C:\Users\geron\OneDrive - UW\Documents\gitRepos\MARC2RDA\Working Documents\Draft Field By Field Spreadsheets\csv"
